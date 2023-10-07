c8f1e1d98904a5c0da2505b708a4671bd747a2e5
Jakob Damgaard Møller <jakobdo@gmail.com>
2023-10-07 10:00:00 +0200

    Uge 5

--- CryptoCurrency/CryptoCurrency/Program.cs
+++ CryptoCurrency/CryptoCurrency/Program.cs
@@ -1,6 +1,17 @@
-﻿public class Converter {
+﻿using System.Collections.Generic;
+
+public class Converter
+{
+    public Dictionary<string, double> Currencies { get; set; }
     //Main method
-    public static void Main(string[] args) {}
+    public static void Main(string[] args)
+    {
+    }
+
+    public Converter()
+    {
+        Currencies = new Dictionary<string, double>();
+    }
 
     /// <summary>
     /// Angiver prisen for en enhed af en kryptovaluta. Prisen angives i dollars.
@@ -9,8 +20,31 @@
     /// </summary>
     /// <param name="currencyName">Navnet på den kryptovaluta der angives</param>
     /// <param name="price">Prisen på en enhed af valutaen målt i dollars. Prisen kan ikke være negativ</param>
-    public void SetPricePerUnit(String currencyName, double price) {
+    public void SetPricePerUnit(String currencyName, double price)
+    {
+        // Prisen kan ikke være negativ!
+        if (price < 0)
+        {
+            throw new System.ArgumentException("Prisen kan ikke være negativ");
+        }
+        // Opdater pris på valutaen, hvis den allerede findes
+        if (Currencies.ContainsKey(currencyName))
+        {
+            Currencies[currencyName] = price;
+        }
+        else
+        {
+            Currencies.Add(currencyName, price);
+        }
+    }
 
+    public double GetPricePerUnit(String currencyName)
+    {
+        if (Currencies.ContainsKey(currencyName) == false)
+        {
+            throw new System.ArgumentException($"Valutaen findes ikke: {currencyName}.");
+        }
+        return Currencies[currencyName];
     }
 
     /// <summary>
@@ -22,7 +56,11 @@
     /// <param name="toCurrencyName">Navnet på den valuta, der konverteres til</param>
     /// <param name="amount">Beløbet angivet i valutaen angivet i fromCurrencyName</param>
     /// <returns>Værdien af beløbet i toCurrencyName</returns>
-    public double Convert(String fromCurrencyName, String toCurrencyName, double amount) {
-        return 0;
+    public double Convert(String fromCurrencyName, String toCurrencyName, double amount)
+    {
+        double fromCurrencyPrice = GetPricePerUnit(fromCurrencyName);
+        double toCurrencyPrice = GetPricePerUnit(toCurrencyName);
+        double totalCurrencyPriceFrom = fromCurrencyPrice * amount;
+        return totalCurrencyPriceFrom / toCurrencyPrice;
     }
 }
--- CryptoCurrency/CryptoCurrency/ProgramTest.cs
+++ CryptoCurrency/CryptoCurrency/ProgramTest.cs
@@ -1,6 +1,59 @@
 using Xunit;
 
-public class ProgramTest {
+public class ProgramTest
+{
+    [Fact]
+    public void TestSingleSetPricePerUnit()
+    {
+        // Arrange
+        Converter convert = new();
 
-    
+        // Act
+        convert.SetPricePerUnit("BTC", 1000);
+
+        // Assert
+        Assert.Equal(1000, convert.GetPricePerUnit("BTC"));
+    }
+
+    [Fact]
+    public void TestLastSetPricePerUnitCounts()
+    {
+        // Arrange
+        Converter convert = new();
+
+        // Act
+        convert.SetPricePerUnit("BTC", 1000);
+        convert.SetPricePerUnit("BTC", 2000);
+
+        // Assert
+        Assert.Equal(2000, convert.GetPricePerUnit("BTC"));
+    }
+
+    [Fact]
+    public void TestConvertFromCurrencyNameExists()
+    {
+        // Arrange
+        Converter convert = new();
+        convert.SetPricePerUnit("BTC", 1000);
+        convert.SetPricePerUnit("LTC", 100);
+
+        // Act
+        double result = convert.Convert("BTC", "LTC", 10);
+
+        // Assert
+        Assert.Equal(100, result);
+    }
+
+    [Fact]
+    public void TestConvertFromCurrencyNameDoesNotExists()
+    {
+        // Arrange
+        Converter convert = new();
+        convert.SetPricePerUnit("BTC", 1000);
+        convert.SetPricePerUnit("LTC", 100);
+
+        // Act & Assert
+        Assert.Throws<ArgumentException>(() => convert.Convert("BTC_NOT", "LTC", 10));
+    }
+
 }
